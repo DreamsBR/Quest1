fa2f0464cc67354383f64706b01d87bfb5042f14
Marcos Basurto <marcosbr301@gmail.com>
2023-12-29 04:00:35 -0500

    Tarae Bucles

---
+++ src/EjerciosTarea.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class EjerciosTarea {
+    /*
+    System.out.println("Circle Area : "+Quest.circleArea(10));
+    System.out.println("Temperature : "+Quest.temperatureCelsiusToFahrenheit(10)+"°F");
+    System.out.println("Volumen Sfera : "+Quest.esferVolume(10));
+    System.out.println("Operaciones Matemaricas:");
+    Quest.matematicsOperation(10 , 30);
+    Quest.exchangesMoney(122);
+*/
+    Scanner scanner = new Scanner(System.in);
+      /*
+
+        System.out.println("Ingrese un numero");
+        int cont = 0;
+        int num = scanner.nextInt();
+        while (num != 1){
+            num = num %2!=0 ? (num = 3 * num + 1) : (num = num/2);
+            System.out.println("Numero "+num);
+            cont++;
+        }
+        System.out.println("Operaciones :" + cont);
+        System.out.println("Fin") ;
+
+
+        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        Quest.numbers(array,1);
+
+       */
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,65 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Circle Area : "+Quest.circleArea(10));
-        System.out.println("Temperature : "+Quest.temperatureCelsiusToFahrenheit(10)+"°F");
-        System.out.println("Volumen Sfera : "+Quest.esferVolume(10));
-        System.out.println("Operaciones Matemaricas:");
-        Quest.matematicsOperation(10 , 30);
+
+        /*
+        //Ejercicio
+        for (int i = 1; i <= 50; i++) {
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        } 1
 
 
-        Quest.exchangesMoney(122);
+        //Ejercicio 2
+
+        int count = 0;
+        for (int i = 1; i < 100; i++) {
+            if(i % 2 != 0) {
+                count += i;
+            }
+        }
+        System.out.println(count);
+
+
+        //Ejercicio 3
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(5 +" x " + i +" = "+ 5 * i);
+        }
+
+        //Ejercicio 4
+        int factorial = 1;
+        for (int i = 1; i <= 5; i++) {
+            factorial = factorial*i;
+        }
+        System.out.println(factorial);
+
+        //Ejercicio 5
+            Scanner sc = new Scanner(System.in);
+            int n, suma, cifra;
+            System.out.print("Introduce numero ");
+            n = sc.nextInt();
+            suma = 0;
+
+            while (n != 0) {
+                cifra = n % 10;
+                suma = suma + cifra;
+                n = n / 10;
+            }
+            System.out.println(suma);
+    */
+        //Ejercicio 6
+
+
+
+
     }
+
+
 }
